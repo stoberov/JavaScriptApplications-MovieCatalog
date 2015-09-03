c63a53b68f7b4b7a92d70d72ae6151350f7f68e6
Maskln <lyubomir.nanev@gmail.com>
2015-09-03 19:22:12 +0300

    Slider, filtering movies by duration Added :)

--- assets/js/filtering-year-slider.js
+++ assets/js/filtering-year-slider.js
@@ -2,7 +2,7 @@ $(function () {
     $("#slider").slider({
         min: 1900,
         max: 2015,
-        value: 2000,
+        value: 1950,
         slide: function (event, ui) {
             $(".year").val(ui.value);
         }
